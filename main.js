219f8b84c496fdfe854e381b0ee1c4ef26645147
Warren Gates <warrenckhan@gmail.com>
2019-08-05 20:48:47 -0400

    setup update for client

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
-
+if (require('electron-squirrel-startup')) return app.quit();
+require('update-electron-app')({
+  repo'Moikapy/Webtoons-Desktop',
+  updateInterval: '1 hour',
+  logger: require('electron-log')
+})
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
